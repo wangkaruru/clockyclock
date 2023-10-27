501e224e1d6205f66f34bb5be65f1c200db9339c
Wangare <mac@MACs-MacBook-Air.local>
2023-10-27 14:12:19 +0300

    first commit

---
+++ src/Clock/Clock.js
@@ -0,0 +1,25 @@
+import {React, useState} from 'react';
+import './clock.css';
+
+
+const Clock = () => {
+
+    let time = new Date().toLocaleTimeString();
+
+    const [currentTime, setCurrentTime] = useState(time);
+
+    const updateTime = () => {
+
+        //let time = new Date().toLocaleTimeString();
+        setCurrentTime(time);
+    }
+
+    setInterval(updateTime, 1000);
+  return (
+    <div className="clock">
+        <h1>{currentTime}</h1>
+    </div>
+  )
+}
+
+export default Clock;
